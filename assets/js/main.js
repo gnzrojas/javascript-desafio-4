7753b21ccf06176671fa8a94f099efeeb15acb04
GonzaloRojas <gonzalo.edurojas@gmail.com>
2024-12-14 19:58:46 -0300

    funcion para renderizar 3 propiedades en index

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ import { arriendos } from "./alquiler.js"
 import { ventas } from "./venta.js"
 
 //Función para renderizar las propiedades en base a alquiler o venta, con su respectivo id
-function renderizarPropiedades(propiedades, contenedorId) {
+const renderizarPropiedades = (propiedades, contenedorId) => {
     const contenedor = document.getElementById(`${contenedorId}`)
     if (!contenedor) return
 
@@ -35,7 +35,53 @@ function renderizarPropiedades(propiedades, contenedorId) {
 
 }
 
+//Función para renderizar solo 3 propiedades en Index
+const renderizarIndex = (propiedadesIndex, idIndex, limite = 3) => {
+
+    const contenedorIndex = document.getElementById(idIndex)
+    if(!contenedorIndex) return;
+    
+    let contenidoHtml = '';
+
+    for (let i = 0; i < propiedadesIndex.length && i < limite; i++) {
+        const prop = propiedadesIndex[i];
+
+        contenidoHtml += `
+        <div class="col-md-4 mb-4">
+            <div class="card">   
+            <img src=${prop.src} class="card-img-top" alt="Imagen de la propiedad"/>
+            <div class="card-body">
+            <h5 class="card-title"> ${prop.nombre}</h5>
+            <p class="card-text"> ${prop.descripcion}</p>
+            <p>
+                <i class="fas fa-map-marker-alt"></i> ${prop.ubicacion}</p>
+            <p>
+                <i class="fas fa-bed"></i> ${prop.habitaciones} |
+                <i class="fas fa-bath"></i> ${prop.baños}
+                </p>
+            <p>
+                <i class="fas fa-dollar-sign"></i> ${prop.costo.toLocaleString()}</p>
+            <p class= ${prop.smoke ? 'text-success' : 'text-danger'}>
+                <i class="fa-solid ${prop.smoke ? 'fa-smoking' : 'fa-ban'}"></i> ${prop.smoke ? 'Permitido fumar' : 'No se permite fumar'}</p>
+            <p class= ${prop.pets ? 'text-success' : 'text-danger'}>
+                <i class="fa-solid ${prop.pets ? 'fa-paw' : 'fa-ban'}"></i> ${prop.pets ? 'Se admiten mascotas' : 'No se admiten mascotas'}
+                </p>
+            </div>
+            </div>
+            </div>
+        `;
+
+        
+    }
+
+    contenedorIndex.innerHTML = contenidoHtml
+}
+
 //Ejecutar la función para propiedades en arriendo y en ventas
 renderizarPropiedades(arriendos, 'propiedades-alquiler')
 renderizarPropiedades(ventas, 'propiedades-venta')
+
+//Ejecutar función para propiedades en index
+renderizarIndex(ventas, 'ventaIndex')
+renderizarIndex(arriendos, 'alquilerIndex')
 
